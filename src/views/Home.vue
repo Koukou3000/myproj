12e2156aa9d30338c7e08a1afeeddb7df1c48a0f
koukou3000 <1968204044@qq.com>
2024-05-26 10:43:20 +0800

    axios mock installed

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <h2>Home Page</h2>
+    <el-button @click="getUser">getUser</el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import mock from '@/mock/mock.js'
+
+export default {
+  name: 'Home',
+  methods: {
+    getUser() {
+      axios.get('http://localhost:8080/user')
+        .then(res => {
+        alert(JSON.stringify(res.data))
+      })
+    }
+  }
+}
+</script>
